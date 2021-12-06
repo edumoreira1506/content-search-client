28c7d268b729d1d7cda6de394d2ce131caa89811
Eduardo Moreira <contato@eduardoem.com.br>
2021-12-05 22:37:34 -0300

    Remove default structure

---
+++ src/ContentSearchClient.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosInstance } from 'axios';
+import { RequestErrorHandler } from '@cig-platform/decorators';
+import { IBreeder } from '@cig-platform/types';
+
+interface RequestSuccess {
+  ok: true;
+}
+
+export interface GetBreedersSuccess extends RequestSuccess {
+  breeders: IBreeder[];
+}
+
+export default class ContentSearchClient {
+  private _axiosBackofficeBffInstance: AxiosInstance;
+
+  constructor(contentSearchUrl: string) {
+    this._axiosBackofficeBffInstance = axios.create({
+      baseURL: contentSearchUrl,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH',
+      }
+    });
+  }
+
+  @RequestErrorHandler([])
+  async getBreeders(keyword = '') {
+    const { data } = await this._axiosBackofficeBffInstance.get<GetBreedersSuccess>(
+      `/v1/breeders?keyword=${keyword}`, 
+    );
+
+    return data;
+  }
+}
