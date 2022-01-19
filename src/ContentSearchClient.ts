c8c0ad233493366764a466fa498d87675e7f6486
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-19 20:04:51 -0300

    Bump to 0.0.6

--- src/ContentSearchClient.ts
+++ src/ContentSearchClient.ts
@@ -23,7 +23,7 @@ export interface GetBreederSuccess extends RequestSuccess {
 }
 
 export interface GetPoultrySuccess extends RequestSuccess {
-  poultry: IPoultry & { images: IPoultryImage[] }[];
+  poultry: IPoultry & { images: IPoultryImage[] };
   registers: IPoultryRegister[];
   advertisings: IAdvertising[];
   vaccines: IPoultryRegister[];
